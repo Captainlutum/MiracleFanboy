48c81b4cc3fcf6b5b7fba3bae84cc556f5db0c39
nutchanon <tumkubeiei@gmail.com>
2019-02-26 12:19:21 +0700

    Initial commit

---
+++ app/src/main/java/nutchanon/kusonprempool/bsru/ac/th/MainActivity.java
@@ -0,0 +1,26 @@
+package nutchanon.kusonprempool.bsru.ac.th;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       //   Add Fragment to Activity
+        getSupportFragmentManager().beginTransaction().add(R.id.layoutMainFragment, new MainFragment()).commit();
+
+
+
+
+
+
+
+
+
+    }   //Main Method
+
+}   //Main Class
